da322cc2d1f4614a666ea86ff91dd4d791a6a6aa
YarlinVargas <yxvargash@unal.edu.co>
2024-04-07 14:47:03 -0500

    se agrega imagen a productos

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,20 +2,6 @@ import sql from "mssql";
 import { DB_DATABASE, DB_PASSWORD, DB_SERVER, DB_USER } from "../config.js";
 
 
-var config = {  
-  server: 'YARLIN\SQLEXPRESS',
-  authentication: {
-      type: 'default',
-      options: {
-          userName: 'admin',
-          password: '123'
-      }
-  },
-  options: {
-      database: 'autotech',
-      port: 3000  //your port number
-  }
-}; 
 export const dbSettings = {
   user: DB_USER,
   password: DB_PASSWORD,
@@ -29,7 +15,7 @@ export const dbSettings = {
 
 export const getConnection = async () => {
   try {
-    const pool = await sql.connect(config);
+    const pool = await sql.connect(dbSettings);
     return pool;
   } catch (error) {
     console.error(error);
